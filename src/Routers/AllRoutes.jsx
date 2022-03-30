170322655acd368d6c919c492e338a3b434213e1
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-30 13:13:42 +0530

    created SearchPage

--- src/Routers/AllRoutes.jsx
+++ src/Routers/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from '../Components/Home/Home'
 import Cart from '../Components/Cart/Cart'
+import SearchPage from '../Components/SearchPage/SearchPage'
 const AllRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/cart' element={<Cart />} />
+            <Route path='/searchpage/:id' element={<SearchPage />} />
         </Routes>
     )
 }
