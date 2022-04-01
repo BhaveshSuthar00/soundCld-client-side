5815ec493c02e846f303d9012246368404210ede
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-01 14:06:31 +0530

    added cart in search page

--- src/Components/SearchPage/RightSide/Right.jsx
+++ src/Components/SearchPage/RightSide/Right.jsx
@@ -13,6 +13,51 @@ const RightWrapper = styled.div`
     }
     &>div {
         height: 150px;
+        border: 1px solid #dbdbdb;
+        display: flex;
+        width: 99%;
+        padding: 0.5% 0;
+        margin: auto;
+        margin-bottom: 2%;
+        &>div:first-child {
+            width: 15%;
+            height : 100%;
+            &>img { 
+                width: 100%;
+                height: 100%;
+            }
+        }
+        &>.rightCorner {
+            display: flex;
+            padding-top: 1%;
+            margin-left: 2%;
+            text-align: left;
+            width: 80%;
+            &>.play_pause_icon {
+                width: 5%;
+                &>svg {
+                    border-radius: 50%;
+                    display: block;
+                    font-size: 41px;
+                    color: #ff5500;
+                }
+            }
+            &>.singer_name {
+                width: 100%;
+                display: flex;
+                &>.singer_main {
+                    width: 95%;
+                }
+                &>.category {
+                    width: 5%;
+                    padding: 1% 1% 2% 1%;
+                    border-radius: 10px;
+                    background-color: #bbbbbb;
+                    height: 10%;
+                    color: white;
+                }
+            }
+        }
     }
 `
 
--- src/Components/SearchPage/RightSide/RightSide.jsx
+++ src/Components/SearchPage/RightSide/RightSide.jsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import { RightWrapper } from './Right'
 
+import CardRight from './CardRight';
+
 const RightSide = ({page, query}) => {
   const [singerData, setSingerData ] = useState([]);
   const getData = async (query) => {
@@ -13,20 +15,24 @@ const RightSide = ({page, query}) => {
         if(el.singer.toLowerCase() === query.toLowerCase()) return true;
         else return false;
       })
-      setSingerData(...data)
-      console.log(data.length, data)
+      setSingerData(data)
     } catch (e) {
       console.log(e)
     }
   }
   useEffect(() => {
     getData(query)
-    // axios.get('https://soundcloud-serverside.herokuapp.com/')
   }, [query,page]);
+  if(singerData.length === 0) {
+    return <>Loading...</>
+  }
   return (
     <RightWrapper>
-      <div>
-      </div>
+      {
+        singerData.map((elem, i) =>{
+          return <CardRight key={i} elem={elem} index={i} />
+        })
+      }
     </RightWrapper>
   )
 }
