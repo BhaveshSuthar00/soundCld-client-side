1c8807a4e66d286144eaef4fa5964ca0ba49e2b2
bhavesh suthar <bhavesh.chandraprakash@influx.co.in>
2024-12-12 23:52:54 +0530

    modify packages

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BaseURL = 'https://erin-tired-jackrabbit.cyclic.app';
+export const BaseURL = 'https://soundcloud-server-side.onrender.com';
 // export const BaseURL = 'http://localhost:2000';
